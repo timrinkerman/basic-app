6a7615403ea01d26f48ef817a8ffa4236a52c990
timrinkerman <tim.rinkerman98@gmail.com>
2020-12-14 20:00:00 -0600

    some refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, {useState, useEffect} from "react"
 import './App.css';
+import { useForm } from "react-hook-form"
 
 function App() {
   const [count, setCount] = useState(0)
   const [joke, setJoke] = useState("")
-
+  const {register, handleSubmit, errors} = useForm()
+  
+  const onSubmit = (data) => {
+    console.log(data)}
+  
+  
   useEffect(() => {
     
     fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/jokes/random", { method: "GET",
@@ -17,12 +23,19 @@ function App() {
     .then(json => setJoke(json.text))
    
   }, [count])
-  return (
+  
+    return (
     <div>
-      <button onClick={() =>setCount(count + 1)}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+          <input ref={register} required name="image" type="file"/>
+          <button>Submit</button>
+        </form>
+      {/* <button onClick={() =>setCount(count + 1)}>
       You've read {count} jokes
       </button>
-    {joke}</div>
+    {joke} */}
+    </div> 
+   
    
 
   );
