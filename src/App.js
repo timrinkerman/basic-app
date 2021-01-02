4517bff97197dd7e0efe896160679c13ea1c6c4d
timrinkerman <tim.rinkerman98@gmail.com>
2021-01-02 12:04:39 -0600

    basic app to demo useEffect/useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react"
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [joke, setJoke] = useState("")
+
+  useEffect(() => {
+    
+    fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/jokes/random", { method: "GET",
+    headers: {
+      "x-rapidapi-key": "6769fc4edbmsh022653918a3cbd9p1d66ddjsne185340ac03d",
+      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+      "useQueryString": true
+    }})
+    .then(res =>res.json())
+    .then(json => setJoke(json.text))
+   
+  }, [count])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <button onClick={() =>setCount(count + 1)}>
+      You've read {count} jokes
+      </button>
+    {joke}</div>
+   
+
   );
 }
 
